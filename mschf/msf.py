2d6ddaecd7552029fe84c73a028ef30e4fe79bd9
pshouse <shouse.patrick@gmail.com>
2019-02-01 12:58:26 -0500

    file open, new window

--- mschf/msf.py
+++ mschf/msf.py
@@ -1,16 +1,52 @@
 import toga
 import os
 from durus.connection import Connection
+from urllib.parse import quote
+from toga.style import Pack
 
+class MsfWindow(toga.Window):
+    def __init__(self, msf):
+        self.msf = msf
+        title = os.path.splitext(os.path.basename(msf.filename))[0]
+
+        super().__init__(
+            title=title,
+            position=(200,200),
+            size=(984, 576),
+            closeable=True
+        )
+        self.create()
+
+    def create(self):
+        self.html_view = toga.WebView(
+            style=Pack(
+                flex=1,
+                width=984,
+                height=576
+            ),
+            #on_key_down=self.msf.on_key_press
+        )
+        self.content = self.html_view
+    
+    def redraw(self):
+        pass
+        #self.html_view.set_content(self.msf.fileURL, 'test content')
+        
 class MSF(toga.Document):
     db = None
 
     def __init__(self, filename, app):
         super().__init__(filename=filename, document_type='Mschf Storage Facility', app=app)
-    
+        self.window = MsfWindow(self)
+        self.window.app = self.app
+
     def read(self):
         if os.path.isfile(self.filename):
             self.db = Connection(self.filename)
     
     def show(self):
-        pass
+        self.window.redraw()
+        self.window.show()
+
+    def fileURL(self):
+        return 'file://{}'.format(quote(self.filename))
